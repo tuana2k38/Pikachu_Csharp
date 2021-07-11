22ab0e0f65d0082e62af721b8ef72fde383f9e68
HuyTuan <tuana2k38@gmail.com>
2021-07-11 09:22:10 +0700

    Create Game project

---
+++ pikachu/pikachu/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pikachu
+{
+    public partial class Form1 : Form
+    {
+        private bool flag = true;
+        private static GameModel myGameModel = new GameModel(20, 16, 36);
+        public PictureBox[,] px = new PictureBox[myGameModel._height, myGameModel._width];
+        struct _point
+        {
+            public int row, col;
+        }
+        _point _point1 = new _point();
+        _point _point2 = new _point();
+        public Form1()
+        {
+            InitializeComponent();
+            
+            this.Height = myGameModel._height * 65 + 53;
+            this.Width = myGameModel._width * 53 + 33;
+            for (int i = 0; i < myGameModel._height; i++)
+            {
+                for (int j = 0; j < myGameModel._width; j++)
+                {
+                    px[i,j] = new PictureBox();
+                    px[i,j].Width = 53; px[i,j].Height = 65;
+                    px[i,j].Top = 10 + i * 65; px[i,j].Left = 10 + j * 53;
+                    try
+                    {
+                        string name = "pic" + myGameModel.getCell(i,j).ToString() + ".jpg";
+                        px[i,j].Image = Image.FromFile(name);
+                        this.Controls.Add(px[i,j]);
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    
+                    px[i, j].Name = i.ToString() + "x" + j.ToString();
+                    px[i,j].Click += new EventHandler(pictureBoxClickEventhandle);
+                    px[i,j].MouseHover += new EventHandler(pictureBoxMouseHoverEventhandle);
+                    px[i,j].MouseLeave += new EventHandler(pictureBoxMouseLeaveEventhandle);
+                }
+            }
+        }
+        public void pictureBoxMouseLeaveEventhandle(object sender, EventArgs e)
+        {
+            
+        }
+        public void pictureBoxMouseHoverEventhandle(object sender, EventArgs e)
+        {
+            
+        }
+        
+        public void pictureBoxClickEventhandle(object sender, EventArgs e)
+        {
+            var control  = (Control)sender;
+            var x = int.Parse(control.Name.Split('x')[0]);
+            var y = int.Parse(control.Name.Split('x')[1]);
+            makeClickedCell(sender as PictureBox);
+            flag = !flag;
+            if (!flag)
+            {
+                _point1.row = x;
+                _point1.col = y;
+                
+            }
+            else
+            {
+                _point2.row = x;
+                _point2.col = y;
+                if (myGameModel.CheckWid(_point1.row, _point1.col, _point2.row, _point2.col))
+                {
+                    (sender as PictureBox).Image = null;
+                    px[_point1.row, _point1.col].Image = null;
+                }
+                else if (myGameModel.CheckHei(_point1.row, _point1.col, _point2.row, _point2.col))
+                {
+                    (sender as PictureBox).Image = null;
+                    px[_point1.row, _point1.col].Image = null;
+                }
+                else if (myGameModel.CheckCom(_point1.row, _point1.col, _point2.row, _point2.col))
+                {
+                    (sender as PictureBox).Image = null;
+                    px[_point1.row, _point1.col].Image = null;
+                }
+            }
+        }
+
+        public void makeClickedCell(PictureBox px)
+        {
+            float ratio = 0.27f;
+
+            //convert image in picture to bitmap
+            Bitmap bmp = (Bitmap)px.Image;
+
+            for (int w = 0; w < bmp.Width; w++)
+            {
+                for (int h = 0; h < bmp.Height; h++)
+                {
+                    // get the pixel at position (w, h) of image
+                    Color c = bmp.GetPixel(w, h);
+
+                    // create new pixel
+                    Color newC = Color.FromArgb((int)(ratio * c.A), (int)(ratio * c.R), (int)(ratio * c.G), (int)(ratio * c.B));
+
+                    // change the value of pixel at posion (w, h)
+                    bmp.SetPixel(w, h, newC);
+                }
+            }
+
+            // update image in picture box
+            px.Image = bmp;
+        }
+    }
+}
---
+++ pikachu/pikachu/GameModel.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pikachu
+{
+    class GameModel
+    {
+        private int[, ] table;
+        private int width, height;
+
+        public GameModel(int _width, int _height, int _numOfType)
+        {
+            width = _width;
+            height = _height;
+
+            table = new int[height, width];
+
+            // su dung de luu cac o da sinh ra pokemon
+            HashSet<int> cellIndex = new HashSet<int>();
+
+            // su dung de sinh so ngau nhien
+            Random random = new Random();
+
+            for (int i=0; i<width*height/2; i++)
+            {
+                // sinh ngau nhien 1 loai pokemon, nam trong khoang tu 1 -> _numOfType
+                int typeOfPokemon = random.Next(1, _numOfType + 1);
+                Console.WriteLine(i);
+                // sinh ra o thu 1 chua pokemon, yeu cau la khong dc nam trong cellIndex
+                int cell1 = random.Next(random.Next(0, width * height+1));
+                while (cellIndex.Contains(cell1))
+                    cell1 = random.Next(random.Next(0, width * height+1));
+                table[cell1 / width, cell1 % width] = typeOfPokemon;
+                cellIndex.Add(cell1);
+
+                // sinh ra o thu 2 chua pokemon, yeu cau la khong dc nam trong cellIndex
+                int cell2 = random.Next(random.Next(0, width * height+1));
+                while (cellIndex.Contains(cell2))
+                    cell2 = random.Next(random.Next(0, width * height+1));
+                table[cell2 / width, cell2 % width] = typeOfPokemon;
+                cellIndex.Add(cell2);
+
+                Console.WriteLine(i);
+            }    
+        }
+        public int getCell(int row, int col)
+        {
+            int _num = table[row,col];
+
+            return _num;
+        }
+        public int _width
+        {
+            get { return this.width; }
+            set { this.width = value; }
+
+        }
+
+        public int _height
+        {
+            get { return this.height; }
+            set { this.height = value; }
+        }
+        public bool CheckWid(int row1, int col1, int row2, int col2)
+        {
+            if (table[row1, col1] != table[row2, col2])
+                return false;
+            else
+            {
+                if (row1 == 0 && row2 == 0)
+                {
+                    return true;
+                }
+                if (row1 == height - 1 && row2 == height - 1)
+                    return true;
+                if (col1 == 0 && col1 == 0)
+                {
+                    return true;
+                }
+                if (col1 == width -1 && col1 == width - 1)
+                    return true;
+
+                if (col1 < col2)
+                    for (int i = col1; i < col2; i++)
+                    {
+                        if (table[row1, i + 1] != 0 && table[row1, i + 1] != table[row2, col2])
+                            return false;
+                        else if (table[row1, i + 1] == table[row2, col2])
+                        {
+                            table[row1, col1] = table[row2, col2] = 0;
+                            return true;
+                        }
+                    }
+                else
+                    for (int i = col1; i > col2; i--)
+                    {
+                        if (table[row1, i - 1] != 0 && table[row1, i - 1] != table[row2, col2])
+                            return false;
+                        else if (table[row1, i - 1] == table[row2, col2])
+                        {
+                            table[row1, col1] = table[row2, col2] = 0;
+                            return true;
+                        }
+                    }
+                if (row1 == 0 && row2 == 0)
+                {
+                    return true;
+                }
+                if (row1 == height && row2 == height)
+                    return true;
+                if (col1 == 0 && col1 == 0)
+                {
+                    return true;
+                }
+                if (col1 == height && col1 == height)
+                    return true;
+
+                return false;
+            }
+                
+        }
+        public bool CheckHei(int row1, int col1, int row2, int col2)
+        {
+            if (table[row1, col1] != table[row2, col2])
+                return false;
+            else
+            {
+                if (row1 < row2)
+                    for (int i = row1; i < row2; i++)
+                    {
+                        if (table[i + 1, col1] != 0 && table[i + 1, col1] != table[row2, col2])
+                            return false;
+                        else if (table[i + 1, col1] == table[row2, col2])
+                        {
+                            table[row1, col1] = table[row2, col2] = 0;
+                            return true;
+                        }
+                    }
+                else
+                    for (int i = row1; i > row2; i--)
+                    {
+                        if (table[i - 1, col1] != 0 && table[i - 1, col1] != table[row2, col2])
+                            return false;
+                        else if (table[i - 1, col1] == table[row2, col2])
+                        {
+                            table[row1, col1] = table[row2, col2] = 0;
+                            return true;
+                        }
+                    }
+                return false;
+            }
+        }
+        public bool CheckCom(int row1, int col1, int row2, int col2)
+        {
+            if (table[row1, col1] != table[row2, col2])
+                return false;
+            else
+            {
+                if (row1 < row2)
+                {
+                    if (col1 < col2)
+                    {
+                        /* check like as Z char */
+                        for (int i = row1 + 1; i < row2; i++) 
+                        {
+                            if (table[i, col1] != 0)
+                            {
+                                return false;
+                            }
+                            if(table[i, col1 + 1] == 0)
+                            {
+                                for(int j = col1 + 1; j < col2; j++)
+                                {
+                                    if (table[i, j] != 0 && table[i, j + 1] != table[row2, col2])
+                                        return false;
+                                    if (table[i, j + 1] == table[row2, col2])
+                                        return true;
+                                    if (table[i + 1, j] == 0 || table[i + 1, j] == table[row2, col2])
+                                    {
+                                        for (int k = i + 1; k <= row2; k++)
+                                        {
+                                            if (table[k, j] != 0 || table[k, j] != table[row2, col2])
+                                            {
+                                                return false;
+                                            }
+                                            if (table[k, j] == table[row2, col2])
+                                                return true;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        /*check like as L char*/
+                        for(int i = row1 + 1; i <= row2; i++)
+                        {
+                            if (table[i, col1] != 0)
+                            {
+                                return false;
+                            }
+                            else
+                            {
+                                if (table[i, col1 + 1] == table[row2, col2])
+                                    return true;
+                                if(table[i, col1 + 1] == 0)
+                                {
+                                    for(int j=col1 + 2; j <= col2; j++)
+                                    {
+                                        if (table[i, j] == table[row2, col2])
+                                            return true;
+                                        if (table[i, j] != 0)
+                                            return false;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        for (int i = row1 + 1; i < row2; i++)
+                        {
+                            if (table[i, col1] != 0)
+                            {
+                                return false;
+                            }
+                            if (table[i, col1 - 1] == 0)
+                            {
+                                for (int j = col1 - 1; j > col2; j--)
+                                {
+                                    if (table[i, j] != 0)
+                                        return false;
+                                    if (table[i + 1, j] == 0 || table[i + 1, j] == table[row2, col2])
+                                    {
+                                        for (int k = i + 1; k <= row2; k++)
+                                        {
+                                            if (table[k, j] != 0 || table[k, j] != table[row2, col2])
+                                            {
+                                                return false;
+                                            }
+                                            if (table[k, j] == table[row2, col2])
+                                                return true;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+        
+    }
+}
